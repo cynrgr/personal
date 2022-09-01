326ffdd881349c217973b05806282a8976a2b695
Francyne Rigor <111488342+cynrgr@users.noreply.github.com>
2022-09-01 15:26:21 +0800

    Create Pet.java

---
+++ 1st folder/Pet.java
@@ -0,0 +1,11 @@
+public class Pet {
+    String name;
+    int trustlevel;
+    int petlevel;
+    boolean ishappy;
+
+    // Pet Method
+    public void petStatus() {
+        System.out.println("His name is " + name + ". Our trustlevel is " + trustlevel + ". He is always happy. " + ishappy);
+    }
+}
